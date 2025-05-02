8a0b7d2255d9ab646e94817e2d4cafc91c12b698
Ststone <chima.miro@gmail.com>
2025-05-02 17:03:07 +0800

    change host

--- app.py
+++ app.py
@@ -479,4 +479,4 @@ def handle_answer(data):
             next_question(room_id)
 
 if __name__ == '__main__':
-    socketio.run(app, host='127.0.0.1', port=8000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=8000, debug=True)
